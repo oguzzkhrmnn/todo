b145fa08f56e5d725d26cac83de605e9ccb55d98
oguz <oguzkahraman001@gmail.com>
2021-12-06 20:14:44 +0300

    array

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,12 +3,16 @@ import React from 'react';
 
 export default function Todos({ToDoItems}){
     const renderTodos =() =>{
-        
+
         const todos= ToDoItems.map((item) => {
             console.log(item);
             return <li>{item}</li>;
 
         });
+        let arrayy=["oguz","kahraman"];
+        localStorage.setItem("oguz",todos);
+        const oguz=localStorage.getItem("oguz");
+        console.log(oguz);
     return(
         <>
         <ul>{todos}</ul>
