2f65ddec6f1a5be9565e6c711ec2d1d39c99da95
oguz <oguzkahraman001@gmail.com>
2021-12-07 17:05:01 +0300

    ornek

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import {url,year} from "./Api"
+import React, { useEffect } from 'react';
+import { useBootstrapPrefix } from 'react-bootstrap/esm/ThemeProvider';
+import { url, year } from "./Api"
 
 export default function Todos({ ToDoItems }) {
-    const renderTodos = () => {
+    // const renderTodos = () => {
 
-        const todos = ToDoItems.map((item, index) => {
-            console.log(ToDoItems[ToDoItems.length - 1]);
-            return <li>key={item}{index}</li>;
+    //     const todos = ToDoItems.map((item, index) => {
+    //         console.log(ToDoItems[ToDoItems.length - 1]);
+    //         return <li>key={item}{index}</li>;
 
-        });
-        /* const person = {
-             firstName: "John",
-             lastName: "Doe",
-             age: 50,
-             eyeColor: "blue"
-           };
-         localStorage.setItem("oguz",JSON.stringify(person));
-         const oguz=JSON.parse(localStorage.getItem("oguz"));//obje olarak yazdırıyor
-         console.log(oguz);*/
-
+    //     });
+    /* const person = {
+         firstName: "John",
+         lastName: "Doe",
+         age: 50,
+         eyeColor: "blue"
+       };
+     localStorage.setItem("oguz",JSON.stringify(person));
+     const oguz=JSON.parse(localStorage.getItem("oguz"));//obje olarak yazdırıyor
+     console.log(oguz);*/
+    let filters = "asd";
+    useEffect(() => {
+        //bileşen yüklediten hemen sonra çalışılacak kısım
+        localStorage.setItem("oguz", filters)
         fetch(`${url}football_competitions?${year}`)
             .then((response) => response.json())
             .then((cevap) => console.log(cevap))
-        return (
+        return () => {
+            // Bileşenden sonra hemen sonra çalışıcak
+            localStorage.clear();
+        }
+    }, []) // bir kere çalışsın diye boş array 
 
-            <>
-                <ul>{todos}</ul>
-            </>
+    return (
+
+            <></>
         );
-    };
-    return <>{renderTodos()}</>;
+};
+    
 
-}
